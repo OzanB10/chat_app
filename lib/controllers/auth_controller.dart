32417732fa71eea91f5f8b4fb394be927c53b7ed
Ozan <ozanbolel@gmail.com>
2025-09-24 15:27:13 +0300

    Update auth_controller.dart

--- lib/controllers/auth_controller.dart
+++ lib/controllers/auth_controller.dart
@@ -1,8 +1,136 @@
+import 'package:chat_app/models/user_model.dart';
+import 'package:chat_app/routes/app_routes.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:get/get.dart';
-import '../services/auth_services.dart';
+import 'package:chat_app/services/auth_service.dart';
+
 
 class AuthController extends GetxController {
-final AuthService = _authService = AuthService();
-final
+  final AuthService _authService = AuthService();
+  final Rx<User?> _user = Rx<User?>(null);
+  final Rx<UserModel?> _userModel = Rx<UserModel?>(null);
+  final RxBool _isLoading = false.obs;
+  final RxString _error = ''.obs;
+  final RxBool _isinitialized = false.obs;
+  User? get user => _user.value;
+  UserModel? get userModel => _userModel.value;
+  bool get isLoading => _isLoading.value;
+  String get error => _error.value;
+  bool get isAuthenticated => _user.value != null;
 
+
+  @override
+  void onInit() {
+    super.onInit();
+    _user.bindStream(_authService.authStateChanges);
+    ever(_user, _handleAuthStateChange);
+  }
+
+  void _handleAuthStateChange(User? user) {
+    if(user != null) {
+      if(Get.currentRoute!=Approutes.login){
+        Get.offAllNamed(Approutes.login);
 }
+    } else {
+      if(Get.currentRoute!=Approutes.main){
+        Get.offAllNamed(Approutes.main);
+      }
+    }
+    if(!_isinitialized.value){
+      _isinitialized.value = true;
+    }
+  }
+
+  void checkInitialAuthState() async {
+    final currentUser = FirebaseAuth.instance.currentUser;
+    if(currentUser != null){
+      _user.value = currentUser;
+      Get.offAllNamed(Approutes.main);
+    } else {
+      Get.offAllNamed(Approutes.login);
+    }
+    _isinitialized.value = true;
+  }
+
+  Future<void> signInWithEmailAndPassword(String email, String password) async {
+    try {
+      _isLoading.value = true;
+      _error.value ='';
+
+      UserModel? userModel = await _authService.signInWithEmailAndPassword(
+        email, 
+        password
+        );
+      if (userModel != null){
+        _userModel.value = userModel;
+        Get.offAllNamed(Approutes.main);
+      }
+    }catch (e) {
+      _error.value = e.toString();
+      Get.snackbar('Error', 'Failed to sign in');
+      print(e);
+    } finally {
+      _isLoading.value = false;
+    } 
+  }
+
+  Future<void> registerWithEmailAndPassword(
+    String email, 
+    String password, 
+    String displayName
+    ) async {
+    try {
+      _isLoading.value = true;
+      _error.value ='';
+
+      UserModel? userModel = await _authService.registerWithEmailAndPassword(
+        email, 
+        password,
+        displayName
+        );
+      if (userModel != null){
+        _userModel.value = userModel;
+        Get.offAllNamed(Approutes.main);
+      }
+    }catch (e) {
+      _error.value = e.toString();
+      Get.snackbar('Error', 'Failed to Create Account');
+      print(e);
+    } finally {
+      _isLoading.value = false;
+    } 
+  }
+
+  Future<void> signOut() async {
+    try {
+      _isLoading.value = true;
+      await _authService.signOut();
+      _userModel.value = null;
+      Get.offAllNamed(Approutes.login);
+    }catch (e) {
+      _error.value = e.toString();
+      Get.snackbar('Error', 'Failed to sign out');
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+  Future<void> deleteAccount() async {
+    try {
+      _isLoading.value = true;
+      await _authService.deleteAccount();
+      _userModel.value = null;
+      Get.offAllNamed(Approutes.login);
+    }catch (e) {
+      _error.value = e.toString();
+      Get.snackbar('Error', 'Failed to delete account');
+    } finally {
+      _isLoading.value = false;
+    }
+  }
+  
+  void clearError() {
+    _error.value = '';
+  }
+}
+
+  
